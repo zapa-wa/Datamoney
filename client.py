6b50a2a5fc02efa05e6ef76d74e2136e776b3282
ruke <elzapatista11@gmail.com>
2024-11-15 14:59:15 +0100

    Add files via upload

--- client.py
+++ client.py
@@ -5,24 +5,36 @@ import logging
 class ExchangeClient:
     def __init__(self, config):
         self.api_url = config['api_url']
-        self.base_currency = config['base_currency']
-        self.target_currency = config['target_currency']
-        self.amount = config['amount']
+        self.from_currency = input("from Currency: ")
+        self.to_currency = input(" to Currency: ")
+        self.amount = float(input("amount: "))
         self.logger = logging.getLogger("ExchangeClient")
+        self.prove_currency = config['prove_currency']
 
     def get_exchange_rate(self):
-        params = {
-            "base": self.base_currency,
-            "symbols": self.target_currency
-        }
         try:
             self.logger.info("Fetching exchange rate...")
-            response = requests.get(self.api_url, params=params)
-            response.raise_for_status()
+            response = requests.get(self.api_url)
             data = response.json()
-            rate = data['rates'][self.target_currency]
-            self.logger.info(f"Exchange rate fetched successfully: {rate}")
-            return rate
+            if response.status_code != 200:
+                print("Error in exchange rate")
+                return None
+            # Verificar si 'conversion_rates' y la moneda objetivo están en la respuesta
+            if "conversion_rates" in data and self.to_currency in data["conversion_rates"]:
+                if self.from_currency == "USD":
+                    rate = data["conversion_rates"][self.to_currency]
+                    self.logger.info(f"Exchange rate fetched successfully: {rate}")
+                    return rate
+                elif self.from_currency != "USD":
+                    self.prove_currency = "USD"
+                    previous_rate_1 = data["conversion_rates"][self.from_currency]
+                    previous_rate_2 = data["conversion_rates"][self.to_currency]
+                    rate = previous_rate_2 / previous_rate_1
+                    self.logger.info(f"Exchange rate fetched successfully: {rate}")
+                    return rate
+            else:
+                self.logger.error("La respuesta de la API no contiene 'rates' o la moneda objetivo.")
+                return None
         except requests.RequestException as e:
             self.logger.error(f"Failed to fetch exchange rate: {e}")
             return None
--- main.py
+++ main.py
@@ -7,22 +7,29 @@ from service import Service
 
 
 def load_config():
-    with open(os.path.join(os.path.dirname(__file__), "../config/config.yaml"), "r") as file:
+    config_path = os.path.join(os.path.dirname(__file__), "config.yaml")
+    with open(config_path, "r") as file:
         return yaml.safe_load(file)
 
 
 def setup_logging(log_file, log_level):
+    log_dir = os.path.dirname(log_file)
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
     logging.basicConfig(
-        level=log_level,
-        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        level=getattr(logging, log_level.upper(), "INFO"),  # Usa "INFO" si el nivel es incorrecto
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
         handlers=[
-            logging.FileHandler(log_file),
-            logging.StreamHandler()
+            logging.StreamHandler(),
+            logging.FileHandler(log_file, mode='a')  # 'a' para agregar al archivo existente
         ]
     )
-
+    print(f"Logging configurado en el archivo: {log_file}")
 
 def main():
+    print("Welcome to Data Money!")
+    print("The program where you can check your currency exchange ")
     # Load configuration
     config = load_config()
 
@@ -35,11 +42,11 @@ def main():
 
     # Fetch exchange rate and convert currency
     rate = exchange_client.get_exchange_rate()
-    converted_amount = service.convert_currency(config['amount'], rate)
+    converted_amount = service.convert_currency(exchange_client.amount, rate)
 
     if converted_amount is not None:
         print(
-            f"{config['amount']} {config['base_currency']} is equal to {converted_amount:.2f} {config['target_currency']}")
+            f"{exchange_client.amount} {exchange_client.from_currency} is equal to {converted_amount:.2f} {exchange_client.to_currency}")
 
 
 if __name__ == "__main__":
